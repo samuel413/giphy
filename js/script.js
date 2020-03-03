0f50cfd745c8127bfa656ab802b8b35463daf1f2
samuel413 <sapereira@s.sfusd.edu>
2020-03-03 02:19:49 +0000

    gitpod epically keeps us from actually testing things because why would we need to do that

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+let giphy_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+
+let searchresult = $(input).val();
+
 $("#search-button").click(function(){
-  
-  
-  
-});
+    
+    fetch(giphy_api_url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+          console.log(data.data[1].id);    
+})});
 
