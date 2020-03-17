5567a58ef6c81e4690b81b9388c396b93232b9ba
samuel413 <sapereira@s.sfusd.edu>
2020-03-17 01:28:12 +0000

    going to finish makeup work

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@
 
 let giphy_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 
-let searchresult = $(input).val();
-
 $("#search-button").click(function(){
-    
+    let randint = Math.floor(Math.random()*9)
+    let searchresult = $("#search-term").val();
+    let giphy_api_url = "https://api.giphy.com/v1/gifs/search?q=" + searchresult + "&rating=pg&api_key=dc6zaTOxFJmzC";
     fetch(giphy_api_url)
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-          console.log(data.data[1].id);    
+          console.log(data.data[randint].images.original.url);    
 })});
 
